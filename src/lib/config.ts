ca9b3f3a262933d0aeb406f725a0bc7f25f2b8f1
bobbymarc00 <bobbymarco942@gmail.com>
2025-11-17 16:28:19 +0700

    Update default values for Solana configuration

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,5 @@
 import { PublicKey } from "@solana/web3.js";
 
-export const rpcEndpoint = process.env.NEXT_PUBLIC_SOLANA_RPC_ENDPOINT || 'https://rpc.testnet.carv.io/rpc';
-export const programId = process.env.NEXT_PUBLIC_SOLANA_PROGRAM_ID || '8MPuR7zKVk2XQJ7apb1Ksc49oKF4AozvF5huKhwBubCa';
-export const treasuryWallet = process.env.NEXT_PUBLIC_SOLANA_TREASURY_WALLET || '3KLEMtjay3MTvNvtmBq8FNkgvfmw3phPUuvggP2vPo83';
+export const rpcEndpoint = process.env.NEXT_PUBLIC_SOLANA_RPC_ENDPOINT || '';
+export const programId = process.env.NEXT_PUBLIC_SOLANA_PROGRAM_ID || '';
+export const treasuryWallet = process.env.NEXT_PUBLIC_SOLANA_TREASURY_WALLET || '';
